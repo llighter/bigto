6e21a62bcee48fb7d5b5d4e2f386ce84251e2e43
Park, Yunha <hadbsgk@gmail.com>
2017-08-29 17:11:40 +0900

    SamplePhone vo 작성: 그래프 보여주기위함(날짜, 가격)

---
+++ src/main/java/com/big/to/vo/SamplePhone.java
@@ -0,0 +1,21 @@
+package com.big.to.vo;
+
+public class SamplePhone {
+	String date;
+	int price;
+	
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	
+}
